5d51f4cb2849aa7440a916caad0b6a75ed46d3bf
AishwaryaRK <aishwarya.kaneri@gmail.com>
2015-07-24 16:07:55 +0530

    maxScoreWordsSuggester func modified to return list of words

--- MaxScoreWordsSuggester.java
+++ MaxScoreWordsSuggester.java
@@ -6,17 +6,19 @@ import java.util.List;
 import java.util.Map;
 
 public class MaxScoreWordsSuggester {
-	public ArrayList<String> getMaxScoreWords(ArrayList<String> validRackCombinations,
+	
+	public ArrayList<Words> getMaxScoreWords(ArrayList<String> validRackCombinations,
 			Map<String, List<Words>> dictionary) {
 		ArrayList<Words> maxScoreWords = new ArrayList<Words>();
 		for (String validRackWord : validRackCombinations) {
 			maxScoreWords.addAll(dictionary.get(validRackWord));
 		}
 		Collections.sort(maxScoreWords);
-		ArrayList<String> wordSuggestions = new ArrayList<String>();
+		ArrayList<Words> wordSuggestions = new ArrayList<Words>();
 		for (int i = 0; i <= 10 && i < maxScoreWords.size(); i++) {
-			wordSuggestions.add(maxScoreWords.get(i).getWord());
+			wordSuggestions.add(maxScoreWords.get(i));
 		}
 		return wordSuggestions;
 	}
+	
 }
