020e2449dd9f13f9d9fb72c75d93388647bdf7a9
unknown <test@L-MAA-25004061.paypalcorp.com>
2015-07-24 17:12:11 +0530

    adding unit tests for coreDictionaryModule

---
+++ CoreDictionaryTest.java
@@ -0,0 +1,36 @@
+package scrabble.Scrabble_Group1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CoreDictionaryTest {
+
+	CoreDictionary d;
+	@Before
+	public void setUp() {
+		d = new CoreDictionary("testdoc.txt");
+	}
+	
+
+	@Test
+	public void testSortWord() {
+		String testString = "harsha";
+        String testString2 = "ha**sh";
+		assertEquals("aahhrs",d.sortWord(testString));	
+		System.out.println(d.sortWord(testString2));
+		assertEquals("**ahhs",d.sortWord(testString2));
+ 
+	}
+
+	@Test
+	public void testAddToHash() {
+		String testString = "tel";
+		CoreDictionary l = new CoreDictionary("testdoc2.txt");
+		l.addToHash(testString);
+		assertEquals(l.getDictionary().size(),7);
+		
+	}
+
+}
