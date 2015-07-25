c5ce110a18f5b0a536393fda76084ec1becccb96
unknown <banerjee.soumyajyoti4@gmail.com>
2015-07-25 08:38:57 +0530

    WordSuggester: removed getWord funtion

--- WordSuggester.java
+++ WordSuggester.java
@@ -37,12 +37,6 @@ public class WordSuggester {
         return combinationsOfWord;
     }
 
-    private Words getWord(String word){
-        Words w = new Words(word, getScore(word));
-
-        return w;
-    }
-
     public int getScore(String word) {
         int val[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
         int total = 0;
