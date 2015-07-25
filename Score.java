1157eefb5614bafe1263f610507cfbe3ff26f8c2
AishwaryaRK <aishwarya.kaneri@gmail.com>
2015-07-25 11:59:51 +0530

    Update Score.java
    Data members and function made static

--- Score.java
+++ Score.java
@@ -2,10 +2,10 @@ package Scrabble_Group1;
 
 public class  Score{
 
-	private int ScrabbleScores[]={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-	private final int valueofA=97;
+	private static int ScrabbleScores[]={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+	private static final int valueofA=97;
 	
-	public int getScores(String word)
+	public static int getScores(String word)
 	{
 		int total=0;
 		for(int i=0;i<word.length();i++)
